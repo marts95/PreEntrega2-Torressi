90d765a4b698c881a66acc2b6e37bbd672b4759b
mart95 <mar.torressi.sanchez.95@gamil.com>
2023-06-10 16:57:43 -0300

    agregando algunos estilos

--- src/components/common/productCard/ProductCard.jsx
+++ src/components/common/productCard/ProductCard.jsx
@@ -11,7 +11,10 @@ import "./ProductCard.css";
 
 export const ProductCard = ({ elemento }) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      sx={{ maxWidth: 345, height: "550px" }}
+      style={{ backgroundColor: "#3d3d3d", borderRadius: "15px",}}
+    >
       <CardActionArea>
         <CardMedia
           component="img"
@@ -19,17 +22,36 @@ export const ProductCard = ({ elemento }) => {
           image={elemento.img}
           alt={elemento.title}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+        <CardContent style={{height: "180px"}}>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            style={{ color: "#c17767", fontWeight: "550" }}
+          >
             {elemento.title}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            style={{ color: "#ffffff", height: "70px" }}
+          >
             {elemento.description}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            style={{ color: "#ffffff", fontSize: "20px", paddingTop: "1rem" }}
+          >
+            $ {elemento.price}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button color="primary" variant="contained">
+      <CardActions style={{ paddingTop: "0.5rem" }}>
+        <Button
+          style={{ backgroundColor: "#c17767", fontWeight: "600"}}
+          variant="contained"
+        >
           Agregar al carrito
         </Button>
       </CardActions>
