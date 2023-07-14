a18d784d012ce7a5604a0095f9575bcef5d09d34
mart95 <mar.torressi.sanchez.95@gamil.com>
2023-07-14 10:30:34 -0300

    agregando archivo env

--- src/Routes/menuRoutes.js
+++ src/Routes/menuRoutes.js
@@ -1,53 +1,44 @@
-import { CartContainer } from "../components/page/cart/CartContainer"
+import { CartContainer } from "../components/page/cart/CartContainer";
 import { CheckoutContainer } from "../components/page/checkout/CheckoutContainer";
 import { ContactoContainer } from "../components/page/contacto/ContactoContainer";
-import { FirebaseAuth } from "../components/page/firebaseAuth/FirebaseAuth";
 import { Inicio } from "../components/page/inicio/Inicio";
-import { ItemDetailContainer } from "../components/page/itemDetail/ItemDetailContainer"
-import { ItemListContainer } from "../components/page/itemListContainer/ItemListContainer"
+import { ItemDetailContainer } from "../components/page/itemDetail/ItemDetailContainer";
+import { ItemListContainer } from "../components/page/itemListContainer/ItemListContainer";
 
-
- export const menuRoutes = [
-   {
-     id: "inicio",
-     path: "/",
-     Element: Inicio,
-   },
-   {
-     id: "productos",
-     path: "/productos",
-     Element: ItemListContainer,
-   },
-   {
-     id: "contacto",
-     path: "/contacto",
-     Element: ContactoContainer,
-   },
-   {
-     id: "detalle",
-     path: "/item/:id",
-     Element: ItemDetailContainer,
-   },
-   {
-     id: "categorias",
-     path: "/category/:tipo",
-     Element: ItemListContainer,
-   },
-   {
-     id: "carrito",
-     path: "/carrito",
-     Element: CartContainer,
-   },
-   {
-     id: "checkout",
-     path: "/checkout",
-     Element: CheckoutContainer,
-   },
-   
-   {
-     id: "firebase-auth",
-     path: "/firebase-auth",
-     Element: FirebaseAuth,
-   },
-   
- ];
+export const menuRoutes = [
+  {
+    id: "inicio",
+    path: "/",
+    Element: Inicio,
+  },
+  {
+    id: "productos",
+    path: "/productos",
+    Element: ItemListContainer,
+  },
+  {
+    id: "contacto",
+    path: "/contacto",
+    Element: ContactoContainer,
+  },
+  {
+    id: "detalle",
+    path: "/item/:id",
+    Element: ItemDetailContainer,
+  },
+  {
+    id: "categorias",
+    path: "/category/:tipo",
+    Element: ItemListContainer,
+  },
+  {
+    id: "carrito",
+    path: "/carrito",
+    Element: CartContainer,
+  },
+  {
+    id: "checkout",
+    path: "/checkout",
+    Element: CheckoutContainer,
+  },
+];
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,41 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signInWithPopup,
-  GoogleAuthProvider,
-} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZr-OZpGK7vKloN7eiDnrzOTFwhn-N07E",
-  authDomain: "panaderiasanchezbase.firebaseapp.com",
-  projectId: "panaderiasanchezbase",
-  storageBucket: "panaderiasanchezbase.appspot.com",
-  messagingSenderId: "221607190068",
-  appId: "1:221607190068:web:89c02311ec099c0d5c3461",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const dataBase = getFirestore(app);
 
-const autorizacion = getAuth(app);
-
-export const login = async ({ email, contraseña }) => {
-  try {
-    return await signInWithEmailAndPassword(autorizacion, email, contraseña);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const registro = async ({ email, contraseña }) => {
-  return await createUserWithEmailAndPassword(autorizacion, email, contraseña);
-};
-
-const google = new GoogleAuthProvider();
-
-export const loginGoogle = async () => {
-  return await signInWithPopup(autorizacion, google);
-};
